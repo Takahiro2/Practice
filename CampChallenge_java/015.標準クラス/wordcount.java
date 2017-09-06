0666c3f5d4e9408e7254aa8c00e02530b608471b
DESKTOP-QPGBD63\guest1Day <rapid13progress@gmail.com>
2017-09-06 16:32:53 +0900

    課題 文字数の取得　提出します

---
+++ CampChallenge_java/015.標準クラス/wordcount.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+class wordcount {
+
+    public static void main(String[] args) {
+        System.out.println("rapid13progress@gmail.com".substring(15));
+    }
+}
