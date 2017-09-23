344882d071f64782eef73eddb71601109d7cba37
Takahiro-PC\Takahiro <rapid13progress@gmail.com>
2017-09-23 19:08:14 +0900

    課題 タイムスタンプの作成　提出します

--- CampChallenge_java/015.標準クラス/Challenge15a.java
+++ CampChallenge_java/015.標準クラス/Challenge15a.java
@@ -6,10 +6,15 @@ import java.text.SimpleDateFormat;
 class sample {
     public static void main(String[] args) throws ParseException {
         
+        // データ形式を所定のフォーマットにする為のsdfを作成
         SimpleDateFormat sdf =
                 new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         
+        // 2016年1月1日　0時0分0秒をフォーマット形式にしてdayに収納
         Date day = sdf.parse("2016/1/1 0:0:0");
+        
+        // 画面に表示
         System.out.println(day);
+        
     }
 }
