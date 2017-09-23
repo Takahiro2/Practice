ca643a39d9ff8705ada188f6b1354da1c15294a8
Takahiro-PC\Takahiro <rapid13progress@gmail.com>
2017-09-23 15:23:08 +0900

    課題 ユーザー定義メソッドの作成　を提出します

--- CampChallenge_java/011.メソッド/TestServlet.java
+++ CampChallenge_java/011.メソッド/TestServlet.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.mypackage.sample;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,21 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author guest1Day
- */
 public class TestServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     // サーブレットクラス内
     // 自己紹介を表示するメソッド
     void myprofile(PrintWriter pw){
@@ -38,18 +19,12 @@ public class TestServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet TestServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("</body>");
-            out.println("</html>");
+            
+            // 10回表示させる
             int i;
             for(i = 0;i < 10;i++){
             myprofile(out);
+            
             }
         }
     }
