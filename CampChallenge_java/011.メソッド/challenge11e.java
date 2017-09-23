dfd0ed687c603080bec703999738078619529280
Takahiro-PC\Takahiro <rapid13progress@gmail.com>
2017-09-23 17:54:59 +0900

    課題 戻り値2　提出します

--- CampChallenge_java/011.メソッド/challenge11e.java
+++ CampChallenge_java/011.メソッド/challenge11e.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.mypackage.sample;
 
 import java.io.IOException;
@@ -13,54 +8,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author guest1Day
- */
-
 @WebServlet(name = "challenge11e", urlPatterns = {"/challenge11e"})
 public class challenge11e extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    String[] profile(){
-        String[] prof = { "000" , "Watanabe" , "1月25日","千葉県"};
+    // プロフィールを配列にして戻り値として返すメソッド
+    String[] profile() {
+        String[] prof = {"000", "Watanabe", "1月25日", "千葉県"};
         return prof;
     }
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet challenge11e</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet challenge11e at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+            // profileメソッドで返された情報を配列proに収納
+            String[] pro = profile();
             
-           String[] pro = profile(); //{ "000" , "Watanabe" , "1月25日","千葉県"}
-            for(int i =0;i < 4; i++){
-                if(i == 0){
-                    continue;
-                }
-            
-                out.print(pro[i]);
+            // 配列の最初に収納されているID以外を表示
+            for (int i = 1; i < 4;) {
+                out.println(pro[i]);
+                i++;
             }
         }
-       
-            
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
