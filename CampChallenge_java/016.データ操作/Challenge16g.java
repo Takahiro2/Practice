c841e9c0b5e1839db809a8dac92129a540a03660
Takahiro-PC\Takahiro <rapid13progress@gmail.com>
2017-09-24 16:43:17 +0900

    課題 セッションの作成　提出します

--- CampChallenge_java/016.データ操作/Challenge16g.java
+++ CampChallenge_java/016.データ操作/Challenge16g.java
@@ -14,15 +14,21 @@ public class Challenge16g extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        // 文字コードをUTF-8に
         response.setContentType("text/html;charset=UTF-8");
+        
         try (PrintWriter out = response.getWriter()) {
 
-            Date now = new Date();
-            SimpleDateFormat sdf = new SimpleDateFormat();
+            Date now = new Date(); // 現在時刻を代入
+            SimpleDateFormat sdf = new SimpleDateFormat(); // フォーマット作成
+            
+            // セッションんを取得 この処理を先にやらないと前回ログインの情報を取得できない
             HttpSession login = request.getSession();
 
+            // 取得した情報を表示
             out.println(login.getAttribute("lastLogin"));
 
+            // 現在アクセス時刻をセッション"lastLogin"に収納
             login.setAttribute("lastLogin", sdf.format(now));
 
         }
