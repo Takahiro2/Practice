3a5fb169a9435e86699106cbcb47713dd21e7d0e
Takahiro-PC\Takahiro <rapid13progress@gmail.com>
2017-09-24 16:38:00 +0900

    課題 クッキーの記録と表示　提出します

--- CampChallenge_java/016.データ操作/Challenge16f.java
+++ CampChallenge_java/016.データ操作/Challenge16f.java
@@ -14,24 +14,37 @@ public class Challenge16f extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        // 文字コードをUTF-8に
         response.setContentType("text/html;charset=UTF-8");
+
         try (PrintWriter out = response.getWriter()) {
 
+            // Dateに現在時刻を代入 sdfを宣言
             Date now = new Date();
             SimpleDateFormat sdf = new SimpleDateFormat();
+            // Cookieを宣言 "LastLogin"に先ほどの現在時刻を代入
             Cookie login = new Cookie("LastLogin", sdf.format(now));
 
+            // サーバーに先ほどのクッキーを追加、保存 
             response.addCookie(login);
 
+            // 配列Cookieにサーバーに保存されているクッキー情報を代入
+            // 上記で現在時刻を追加しているが、getCookiesメソッドの取得情報は
+            // アクセスした時点での情報を取得する為初回はnullを取得する事になる
             Cookie[] lastLogin = request.getCookies();
 
+            // 中身に情報が入っている時
             if (lastLogin != null) {
+                // lastloginに入っている要素数全てを検索していく .lengthは配列の要素数
                 for (int i = 0; i < lastLogin.length; i++) {
+                    // 配列名"LastLoginを見つけた時
                     lastLogin[i].getName().equals("LastLogin");
-                    out.println(lastLogin[i].getValue());
+                    // 以下のように表示
+                    out.println("最後のログインは" + lastLogin[i].getValue());
                 }
-            } else {
-                out.println("null");
+            } else { // nullの時
+                out.println("null"); // nullと表示
             }
 
         }
